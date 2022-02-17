de0549548bea68fc256def23604f5218f97593f1
ahmed khelifi <ahmedkh3lifi@gmail.com>
2022-02-17 23:37:29 +0100

    todo commit

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <div>
+      <h1>Todolist todotodo ya baba todo</h1>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
